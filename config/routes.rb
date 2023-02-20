d436d190cbf9a8660d07a408dd8b5b8783b17718
629761 <esthermorris775@.com>
2023-02-20 16:56:37 -0500

    Set up authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post "/users" => "users#create"
+  post "/test" => "users#test"
+  post "/sessions" => "sessions#create"
+
 end
