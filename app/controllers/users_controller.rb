3d49ec9758722d0e9753d8182aaf5427ce533e4f
Esther75 <esthermorris775@gmail.com>
2023-03-23 13:56:45 -0400

    user can now add nonprofits to their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# require 'pry';
+
 class UsersController < ApplicationController
 
   def index
@@ -43,37 +45,68 @@ class UsersController < ApplicationController
   def update
 
     user=User.find_by(id: params[:id])
+
     user.name = params[:name] || user.name
     user.email = params[:email] || user.email
     user.profile_picture = params[:profile_picture] || user.profile_picture
     user.location = params[:location] || user.location
     user.bio = params[:bio] || user.bio
     user.monthly_donation_amount = params[:monthly_donation_amount] || user.monthly_donation_amount
-    
-
     user.save
 
-    # params[:my_nonprofits].each do |nonprofit|
-    #     p nonprofit
 
-    #   nonprofit= UserNonprofit.find_by(id: nonprofit)
+  #   p "*" * 60
+  # p user.user_nonprofits
 
-    #     if nonprofit==user.user_nonprofits
-    #       render json: {message: "NonProfit already is in your profile"}
-    #    else 
-    #     UserNonprofit.new(
-    #       user_id: user.id,
-    #       nonprofit_id: nonprofit
-    #       )
-    #       UserNonprofit.save
 
-    #       render json: {user: user.as_json, nonprofits: user.user_nonprofits.as_json }
-    #     end
+    params[:my_new_nonprofit].each do |new_nonprofit|
+      p "*" * 60
+      p new_nonprofit
 
-    #   end
-  
+      if user.user_nonprofits.find_by(nonprofit_id: new_nonprofit)
+        # "Nonprofit exists in your profile already"    
+        p "Do noithing"    
+      else
+        zzz= UserNonprofit.new(
+          user_id: user.id,
+          nonprofit_id: new_nonprofit
+        )
+        zzz.save
+      end
 
+    end
+    
+    render json: {user: user.as_json}
+
+
+# Grab params
+# index through params and compare to existing nonprofits
+# if doesnt exist
+#   add new nonprofit to user prifile 
+
+
+    # params[:my_nonprofits].each do |my_new_nonprofit|
+      # nonprofit= UserNonprofit.find_by(id: nonprofit)
+      # p "*******************my_new_nonprofit***********************************"
+      # p my_new_nonprofit
+      # p "******************************************************"
+      #  user.user_nonprofits .each do |old_nonprofit|
+
+      #   p "************Old Nonprofit******************************************"
+      #   p old_nonprofit.nonprofit_id
+      #   p "******************************************************"
+
+      #   if my_new_nonprofit==old_nonprofit
+      #     render json: {message: "NonProfit already is in your profile"}
+      #  else 
+      #     addnonprofit= UserNonprofit.new(
+      #       user_id: user.id,
+      #       nonprofit_id: my_new_nonprofit
+      #       )
+      #     addnonprofit.save
+      #     # render json: {user: user.as_json, nonprofits: user.user_nonprofits.as_json }
+      #   end
+      # end
+      # end
   end
-
-
 end
